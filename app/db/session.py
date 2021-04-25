2573c490ff94b7753c4c983d65146309838acd81
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-25 16:22:28 +0200

    format

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
