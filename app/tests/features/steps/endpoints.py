5012da6cbdc97b444257ab3ed6c4582e7a7bb874
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-25 18:58:56 +0200

    fix imports

--- app/tests/features/steps/endpoints.py
+++ app/tests/features/steps/endpoints.py
@@ -1,4 +1,4 @@
-from behave import given, use_fixture, use_step_matcher, when, then
+from behave import given, then, use_fixture, use_step_matcher, when
 from faker import Faker
 
 from app.core.config import settings
